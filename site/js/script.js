7118042c208996419ef4c21837ab0c87a5d8187f
Huanyi Chuang <no249a002@gmail.com>
2017-10-25 10:02:13 +0800

    UI improvement

---
+++ site/js/script.js
@@ -0,0 +1,38 @@
+//Switch Price Automatically
+var prc = new Array();
+var theForm = document.forms["shoppingCart"];
+prc["青春還原露"] = 100;
+prc["青春修復霜"] = 200;
+prc["無敵保濕乳"] = 300;
+
+function getPrice(){
+var prodPrice = 0;
+var prod = theForm.elements["sel1"];
+prodPrice = prc[prod.value];
+
+return prodPrice;
+
+}
+function calculateTotal()
+{
+    //Here we get the total price by calling our function
+    //Each function returns a number so by calling them we add the values they return together
+    var cakePrice = getPrice();
+    
+    //display the result
+    var divobj = document.getElementById('price');
+    divobj.style.display='block';
+    divobj.innerHTML = "NT$ "+cakePrice;
+
+}
+function hideTotal()
+{
+    var divobj = document.getElementById('price');
+    divobj.style.display='none';
+}
+
+function passPrice(){
+	getPrice();
+	document.getElementById("priceAmt").value;
+	document.getElementById("priceAmt").value = getPrice();
+}
